76b4b108a287b153f360947f9c336e2e0e4400a9
peller <github@peller.org>
2016-06-01 16:41:39 -0400

    implement selection behavior
    (c) Copyright IBM Corp. 2016

---
+++ selectionWatcher.js
@@ -0,0 +1,15 @@
+define(function() {
+  return function(view) {
+    view.on('mouseover', function(event, item) {
+      debugger;
+    });
+
+    view.on('click', function(event, item) {
+      debugger;
+    });
+
+    view.on('mouseout', function(event, item) {
+      debugger;
+    });
+  };
+});
